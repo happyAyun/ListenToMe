b3e4832431d5f80e9392f3791697f9b2eaff35e9
LeeSeoYoung <lsyoung00@naver.com>
2022-01-26 14:27:23 +0900

    Feat:complete signup signin

---
+++ backend/Listen_to_me/src/main/java/com/ssafy/a605/repository/CounselorRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.a605.repository;
+
+import com.ssafy.a605.model.entity.Client;
+import com.ssafy.a605.model.entity.Counselor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CounselorRepository extends JpaRepository<Counselor,Long> {
+
+    Optional<Counselor> findByEmail(String email);
+    void deleteByEmail(String email);
+
+}
