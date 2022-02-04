494fdd267b27fb335a3298c08bec73ac1a064a07
ayun <shim521@naver.com>
2022-02-04 16:55:26 +0900

    Feat: Bookmarks finish

--- backend/Listen_to_me/src/main/java/com/ssafy/a605/service/BookmarkService.java
+++ backend/Listen_to_me/src/main/java/com/ssafy/a605/service/BookmarkService.java
@@ -1,12 +1,13 @@
 package com.ssafy.a605.service;
 
 import com.ssafy.a605.model.dto.BookmarkDto;
+import com.ssafy.a605.model.entity.Bookmark;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BookmarkService {
-    public List<BookmarkDto> getLikedCounselor(String userEmail, Pageable pageRequest) throws Exception;
-    // public Page<BookmarkDto> getRecommendCounselor(String userEmail, )
+    public List<Bookmark> getLikedCounselor(String userEmail) throws Exception;
+    public boolean setLikeCounselor(String userEmail, String counselorEmail) throws Exception;
 }
