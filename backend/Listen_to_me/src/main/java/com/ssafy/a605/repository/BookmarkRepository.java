e63f772e6b75df081eb3279d3923bed78257ccdd
김유민 <elin0225@naver.com>
2022-02-04 14:42:58 +0900

    Feat: Repository

--- backend/Listen_to_me/src/main/java/com/ssafy/a605/repository/BookmarkRepository.java
+++ backend/Listen_to_me/src/main/java/com/ssafy/a605/repository/BookmarkRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
-    
+    List<Bookmark> findAllByClient_Email(String userEmail);
 }
