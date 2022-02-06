418447482a9950d09fd7deb6c090d915226fc1ec
김유민 <elin0225@naver.com>
2022-02-06 19:13:38 +0900

    Fix : client, counselor information modify

--- backend/Listen_to_me/src/main/java/com/ssafy/a605/service/CounselorService.java
+++ backend/Listen_to_me/src/main/java/com/ssafy/a605/service/CounselorService.java
@@ -5,14 +5,14 @@ import com.ssafy.a605.model.dto.ClientDto;
 import com.ssafy.a605.model.dto.CounselorDto;
 import com.ssafy.a605.model.dto.UserDto;
 import com.ssafy.a605.model.entity.Counselor;
+import com.ssafy.a605.model.response.counselor.CounselorInfoRes;
 
 import java.util.List;
 import java.util.Map;
 
 public interface CounselorService {
-
     public Map<String,String> login(Map<String,String> map) throws Exception;
-    public CounselorDto counselorInfo(String userEmail) throws Exception;
+    public CounselorInfoRes counselorInfo(String userEmail) throws Exception;
     public Counselor getCounselorInfo(String userEmail) throws Exception;
     public boolean joinCounselor(CounselorDto counselorDto) throws Exception;
     public boolean updateCounselor(CounselorDto counselorDto) throws Exception;
@@ -21,5 +21,4 @@ public interface CounselorService {
     public boolean checkId(String userid) throws Exception;
     boolean deleteCounselor(String userEmail) throws Exception;
     List<CertificateDto> getCertificates (String userEmail) throws Exception;
-
 }
