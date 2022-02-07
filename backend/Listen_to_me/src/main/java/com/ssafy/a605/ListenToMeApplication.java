0675206e423f25e0bf02c0340f75746a2d79e01e
김유민 <elin0225@naver.com>
2022-02-07 11:18:16 +0900

    fix: git issue

--- backend/Listen_to_me/src/main/java/com/ssafy/a605/ListenToMeApplication.java
+++ backend/Listen_to_me/src/main/java/com/ssafy/a605/ListenToMeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ListenToMeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ListenToMeApplication.class, args);
     }
-
 }
